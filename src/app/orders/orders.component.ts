547d2608356c72f85c71d3bdca4b675f29c9e164
varsha kakumanu <varshakakumanu@gmail.com>
2022-04-26 12:20:20 +0530

    changes

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder,FormGroup } from '@angular/forms';
-import { OrderModel } from './orders board model';
 import { ApiService } from '../shared/api.service';
+import { OrderModel } from './orders board model';
+
 
 
 @Component({
@@ -16,7 +17,7 @@ export class OrdersComponent implements OnInit {
   api: any;
   orderData!:any;
 
-  constructor(private formbuilder: FormBuilder) { }
+  constructor(private formbuilder: FormBuilder,private service:ApiService) { }
 
   ngOnInit(): void {
     this.formValue = this.formbuilder.group({
@@ -26,21 +27,22 @@ export class OrdersComponent implements OnInit {
       address : [''],
       phoneno : ['']
     })
+    this.getAllOrder();
   }
   postOrderDetails(){
-    this.OrderModelObj.ordernumber = this.formValue.value.ordernumber;
-    this.OrderModelObj.Date =this.formValue.value.Date;
+    this.OrderModelObj.ordernumber = this.formValue.value.orderNumber;
+    this.OrderModelObj.Date =this.formValue.value.date;
     this.OrderModelObj.name =this.formValue.value.name;
     this.OrderModelObj.address =this.formValue.value.address;
-    this.OrderModelObj.phone =this.formValue.value.phone;
+    this.OrderModelObj.phone =this.formValue.value.phoneno;
     //console.log(this.OrderModelObj);
     //this.OrderModelObj.ordernumber = 4;
     //this.OrderModelObj.Date =new Date();
     //this.OrderModelObj.name ='98';
     //this.OrderModelObj.address ='789';
     //this.OrderModelObj.phone ="88";
-    
-    this.api.postOrder(this.OrderModelObj)
+  
+    this.service.postOrder(this.OrderModelObj)
     .subscribe((res: any)=>{
     console.log(res);
     alert("order added successfully")
@@ -54,25 +56,47 @@ export class OrdersComponent implements OnInit {
   })
 }
 getAllOrder(){
-  this.api.getOrder()
+  this.service.getOrder()
   .subscribe((res: any)=>{
     this.orderData= res;   
 
   })
 }
 deleteOrder(row : any){
-  this.api.deleteOrder(row.id)
+  console.log(row,"want")
+  this.service.deleteOrder(row.id)
   .subscribe((res: any)=>{
     alert("order deleted");
     this.getAllOrder();
   })
 }
 onEdit(row: any){
-  this.formValue.controls['orderNumber'].setValue(row.orderNumber);
-  this.formValue.controls['Date'].setValue(row.Date);
-  this.formValue.controls['Name'].setValue(row.Name);
+  console.log(row,"edit")
+  this.formValue.controls['orderNumber'].setValue(row.ordernumber);
+  this.formValue.controls['date'].setValue(row.Date);
+  this.formValue.controls['name'].setValue(row.name);
   this.formValue.controls['address'].setValue(row.address);
-  this.formValue.controls['phoneno'].setValue(row.phoneno);
+  this.formValue.controls['phoneno'].setValue(row.phone);
  
+}
+updateDetails(){
+  this.OrderModelObj.ordernumber = this.formValue.value.orderNumber;
+  this.OrderModelObj.Date =this.formValue.value.date;
+  this.OrderModelObj.name =this.formValue.value.name;
+  this.OrderModelObj.address =this.formValue.value.address;
+  this.OrderModelObj.phone =this.formValue.value.phoneno;
+
+  this.service.updateOrder(this.OrderModelObj,this.OrderModelObj.id+1).subscribe((res: any)=>{
+    console.log(res);
+    alert("updated successfully")
+    let ref = document.getElementById('cancel')
+    ref?.click();
+    this.formValue.reset();
+    this.getAllOrder();
+  },
+      (  _err: any)=>{
+    alert("something went wrong");
+  })
+  this.getAllOrder()
 }
 }
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -21,13 +21,14 @@ export class ApiService {
     }))
   }
   updateOrder(data : any,id:number){
-    return this.http.put<any>("http://localhost:3000/posts"+id,data)
+    console.log(id,data,"update")
+    return this.http.put<any>("http://localhost:3000/posts/"+id,data)
     .pipe(map((res:any)=>{
       return res;
     }))
   }
   deleteOrder(id:number){
-    return this.http.delete<any>("http://localhost:3000/posts"+id)
+    return this.http.delete<any>("http://localhost:3000/posts/"+id)
     .pipe(map((res:any)=>{
       return res;
     }))
